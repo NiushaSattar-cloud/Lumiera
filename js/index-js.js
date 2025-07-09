f5fd61008bd977a56e5adc2a4e7460acdfed4485
Niusha <newshasattar@gmail.com>
2025-07-09 14:45:04 +0330

    9th - started navbar

--- js/index-js.js
+++ js/index-js.js
@@ -10,13 +10,29 @@ const btnYellow = document.getElementById('btn-yellow')
 
 const item1 = document.getElementById('item1')
 const item4 = document.getElementById('item4')
-const overlay1=document.getElementById('overlay1')
-const overlay2=document.getElementById('overlay2')
+const overlay1 = document.getElementById('overlay1')
+const overlay2 = document.getElementById('overlay2')
 
-const title1=document.getElementById('title1')
+const title1 = document.getElementById('title1')
 const line = document.querySelector('.line-with-circles');
 
-const candleInfo = document.getElementsByClassName('.candle-info');
+const candleInfos = document.querySelectorAll('.candle-info');
+const btnPurple = document.getElementById('btn-purple')
+
+const section4 = document.getElementById('section-4')
+const sec4Right = document.getElementById('sec4-right')
+const sec4Title = document.getElementById('sec4-title')
+const sec4P = document.getElementById('sec4-p')
+const btnYellow2 = document.getElementById('btn-yellow2')
+
+const title2 = document.getElementById('title2')
+const line2 = document.getElementById('line-with-circles2')
+const serviceItem = document.querySelectorAll('.service-item');
+const servCircle = document.querySelectorAll('.serv-circle');
+const servTitle = document.querySelectorAll('.serv-title');
+
+const footer = document.getElementById('main-footer')
+
 
 
 
@@ -37,7 +53,7 @@ var nBgColor = "#191919"
 var nMAinColor = "#362664"
 var nComColor = "#D19A0F"
 var nLightColor = "#211E26"
-var nOverlay="#352664b9"
+var nOverlay = "#352664b9"
 
 
 var dBgColor = "#ffffff"
@@ -79,25 +95,48 @@ candleBtn.onclick = function () {
         mainCandle.style.setProperty('background-image', "url('img/candle-off.png')", 'important');
 
         header.style.backgroundColor = nMAinColor
-        header.style.boxShadow="0 0 10px #1b103b"
+        header.style.boxShadow = "0 0 10px #1b103b"
 
         mainTitle.style.color = dLightColor
         mainShortText.style.color = dLightColor
         btnYellow.style.backgroundColor = nComColor
 
-        item1.style.backgroundColor=nMAinColor
-        item4.style.backgroundColor=nLightColor
-        item4.style.color=dLightColor
-        overlay1.style.backgroundColor=nOverlay
-        overlay2.style.backgroundColor=nOverlay
+        item1.style.backgroundColor = nMAinColor
+        item4.style.backgroundColor = nLightColor
+        item4.style.color = dLightColor
+        overlay1.style.backgroundColor = nOverlay
+        overlay2.style.backgroundColor = nOverlay
 
-        title1.style.color=dLightColor
-        line.style.borderColor=nMAinColor
+        title1.style.color = dLightColor
+        line.style.borderColor = nMAinColor
         line.style.setProperty('--D-light-color', nMAinColor);
 
-        // for (let i = 0; i < candleInfo.length; i++) {
-        //     candleInfo[i].style.backgroundColor = nMAinColor;
-        //   }
+        candleInfos.forEach(function (element) {
+            element.style.backgroundColor = nMAinColor;
+        });
+        btnPurple.style.backgroundColor = nMAinColor;
+
+        section4.style.backgroundColor = nMAinColor
+        sec4Right.style.backgroundColor = nLightColor
+        sec4Title.style.color = dLightColor
+        sec4P.style.color = dLightColor
+        btnYellow2.style.backgroundColor = nComColor
+
+        title2.style.color = dLightColor
+        line2.style.borderColor = nMAinColor
+        line2.style.setProperty('--D-light-color', nMAinColor);
+
+        serviceItem.forEach(function (element) {
+            element.style.backgroundColor = nLightColor;
+        });
+        servCircle.forEach(function (element) {
+            element.style.backgroundColor = nMAinColor;
+        });
+        servTitle.forEach(function (element) {
+            element.style.color = dLightColor;
+        });
+
+        footer.style.background = nMAinColor;
 
         on = false
     }
@@ -114,15 +153,43 @@ candleBtn.onclick = function () {
         mainShortText.style.color = dMainColor
         btnYellow.style.backgroundColor = dComColor
 
-        item1.style.backgroundColor=dMainColor
-        item4.style.backgroundColor=dLightColor
-        item4.style.color=dMainColor
-        overlay1.style.backgroundColor=dOverlay
-        overlay2.style.backgroundColor=dOverlay
+        item1.style.backgroundColor = dMainColor
+        item4.style.backgroundColor = dLightColor
+        item4.style.color = dMainColor
+        overlay1.style.backgroundColor = dOverlay
+        overlay2.style.backgroundColor = dOverlay
 
-        title1.style.color=dMainColor
-        line.style.borderColor=dLightColor
+        title1.style.color = dMainColor
+        line.style.borderColor = dLightColor
         line.style.setProperty('--D-light-color', dLightColor);
+
+        candleInfos.forEach(function (element) {
+            element.style.backgroundColor = dMainColor;
+        });
+        btnPurple.style.backgroundColor = dMainColor;
+
+        section4.style.backgroundColor = dMainColor
+        sec4Right.style.backgroundColor = dLightColor
+        sec4Title.style.color = dMainColor
+        sec4P.style.color = dMainColor
+        btnYellow2.style.backgroundColor = dComColor
+
+        title2.style.color = dMainColor
+        line2.style.borderColor = dLightColor
+        line2.style.setProperty('--D-light-color', dLightColor);
+
+        serviceItem.forEach(function (element) {
+            element.style.backgroundColor = dLightColor;
+        });
+        servCircle.forEach(function (element) {
+            element.style.backgroundColor = dMainColor;
+        });
+        servTitle.forEach(function (element) {
+            element.style.color = dMainColor;
+        });
+
+        footer.style.background = dMainColor;
+
 
         on = true
     }
@@ -130,18 +197,109 @@ candleBtn.onclick = function () {
 }
 
 mainCandle.onclick = function () {
+    // وقتی میخوایم خاموش کنیم
     if (on == true) {
         document.body.style.backgroundColor = nBgColor
         candleBtnImg.src = "img/circle-candle-off.png";
         mainCandle.style.setProperty('background-image', "url('img/candle-off.png')", 'important');
+
+        header.style.backgroundColor = nMAinColor
+        header.style.boxShadow = "0 0 10px #1b103b"
+
+        mainTitle.style.color = dLightColor
+        mainShortText.style.color = dLightColor
+        btnYellow.style.backgroundColor = nComColor
+
+        item1.style.backgroundColor = nMAinColor
+        item4.style.backgroundColor = nLightColor
+        item4.style.color = dLightColor
+        overlay1.style.backgroundColor = nOverlay
+        overlay2.style.backgroundColor = nOverlay
+
+        title1.style.color = dLightColor
+        line.style.borderColor = nMAinColor
+        line.style.setProperty('--D-light-color', nMAinColor);
+
+        candleInfos.forEach(function (element) {
+            element.style.backgroundColor = nMAinColor;
+        });
+        btnPurple.style.backgroundColor = nMAinColor;
+
+        section4.style.backgroundColor = nMAinColor
+        sec4Right.style.backgroundColor = nLightColor
+        sec4Title.style.color = dLightColor
+        sec4P.style.color = dLightColor
+        btnYellow2.style.backgroundColor = nComColor
+
+        title2.style.color = dLightColor
+        line2.style.borderColor = nMAinColor
+        line2.style.setProperty('--D-light-color', nMAinColor);
+
+        serviceItem.forEach(function (element) {
+            element.style.backgroundColor = nLightColor;
+        });
+        servCircle.forEach(function (element) {
+            element.style.backgroundColor = nMAinColor;
+        });
+        servTitle.forEach(function (element) {
+            element.style.color = dLightColor;
+        });
+
+        footer.style.background = nMAinColor;
+
         on = false
     }
 
-
+    // وقتی میخوایم روشن کنیم
     else if (on == false) {
         document.body.style.backgroundColor = dBgColor
         candleBtnImg.src = "img/circle-candle-on.png";
         mainCandle.style.setProperty('background-image', "url('img/candle-on1.png')");
+
+        header.style.backgroundColor = dMainColor
+
+        mainTitle.style.color = nMAinColor
+        mainShortText.style.color = dMainColor
+        btnYellow.style.backgroundColor = dComColor
+
+        item1.style.backgroundColor = dMainColor
+        item4.style.backgroundColor = dLightColor
+        item4.style.color = dMainColor
+        overlay1.style.backgroundColor = dOverlay
+        overlay2.style.backgroundColor = dOverlay
+
+        title1.style.color = dMainColor
+        line.style.borderColor = dLightColor
+        line.style.setProperty('--D-light-color', dLightColor);
+
+        candleInfos.forEach(function (element) {
+            element.style.backgroundColor = dMainColor;
+        });
+        btnPurple.style.backgroundColor = dMainColor;
+
+        section4.style.backgroundColor = dMainColor
+        sec4Right.style.backgroundColor = dLightColor
+        sec4Title.style.color = dMainColor
+        sec4P.style.color = dMainColor
+        btnYellow2.style.backgroundColor = dComColor
+
+        title2.style.color = dMainColor
+        line2.style.borderColor = dLightColor
+        line2.style.setProperty('--D-light-color', dLightColor);
+
+        serviceItem.forEach(function (element) {
+            element.style.backgroundColor = dLightColor;
+        });
+        servCircle.forEach(function (element) {
+            element.style.backgroundColor = dMainColor;
+        });
+        servTitle.forEach(function (element) {
+            element.style.color = dMainColor;
+        });
+
+        footer.style.background = dMainColor;
+
+
         on = true
     }
 }
@@ -152,5 +310,3 @@ mainCandle.onclick = function () {
 
 
 
-
-// ایتم محصولات توی حالت دارک رنگ اون چیزی که ظاهر میشه مطابق ورژن روزشه و باید تغییرش بدم
